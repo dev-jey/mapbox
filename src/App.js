c75b65f0136ff97966965b1dbccdd9b54ea913ac
dev-jey <savalijey@gmail.com>
2019-10-22 20:27:19 +0300

    Make the map fullscreen

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2F2YWxpa2l0dWt1IiwiYSI6ImNrMjIxczBhMzA3bWMzY3Fvc2RydzQxd3UifQ.hNGtZUh5syY9E684sH54bg';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-  }
   componentDidMount(){
     new mapboxgl.Map({
       container: 'map',
@@ -16,7 +13,7 @@ class App extends React.Component {
   }
   render(){
   return (
-    <div id="map"  style={{width: "400px", height: "300px"}}>
+    <div id="map"  style={{width: "100%", height: "100vh"}}>
     </div>
   );
   }
